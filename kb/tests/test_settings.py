603b27996b67f9a5b65a99220861e17087a8fa75
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2014-07-21 09:36:20 -0300

    more tests for api_settings

--- kb/tests/test_settings.py
+++ kb/tests/test_settings.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-
 from django.test import TestCase
 
 
@@ -13,3 +12,18 @@ class SettingsTestCase(TestCase):
     def test_default_form_search_class(self):
         from kb.forms import SearchForm
         self.assertEqual(self.settings.DEFAULT_SEARCH_FORM_CLASS, SearchForm)
+
+    def test_default_option(self):
+        from kb.settings import APISettings
+        api_settings = APISettings(None, {'FOO': 'BAR'}, None)
+
+        self.assertEqual(api_settings.FOO, 'BAR')
+
+    def test_non_exists_option_should_fail(self):
+        self.assertRaises(AttributeError, getattr, self.settings, 'FOO')
+
+    def test_import_non_exist_class_should_fail(self):
+        from kb import settings
+        api_settings = settings.APISettings({'DEFAULT_SEARCH_FORM_CLASS': 'path.to.non.exist.class'},
+                                            settings.DEFAULTS, settings.IMPORT_STRINGS)
+        self.assertRaises(ImportError, getattr, api_settings, 'DEFAULT_SEARCH_FORM_CLASS')
