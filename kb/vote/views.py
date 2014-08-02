ffc4a80975e888fec7ae9a75c05e03a749d351a5
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2014-08-02 15:43:21 -0300

    improve json vote test

--- kb/vote/views.py
+++ kb/vote/views.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
+import json
+
 from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import get_object_or_404
+from django.utils.translation import ugettext as _
 
 from ..article.models import Article
 
@@ -11,6 +14,7 @@ def vote(request, slug, vote):
     article.votes.add(request._secret_token, vote)
 
     if request.is_ajax():
-        return HttpResponse('{"success": true}', content_type='application/json')
+        response = {'response': _('Thank you for your feedback.')}
+        return HttpResponse(json.dumps(response), content_type='application/json')
 
     return HttpResponsePermanentRedirect(article.get_absolute_url())
