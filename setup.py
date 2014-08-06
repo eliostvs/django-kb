f2ba7df4db4bb4f46ddbabab50ecc33be10d5862
Elio E. Duarte <elio.esteves.duarte@gmail.com>
2014-08-05 21:50:30 -0300

    change development status classifer from pre-alpha to beta

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     zip_safe=False,
     keywords='django,knowledge base',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
